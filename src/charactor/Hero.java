b7a3f7232b2890ac1d6bd719a6ed0c7897c0244a
周敏 <3295227649@qq.com>
2020-05-22 17:52:33 +0800

    test

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -10,12 +10,16 @@ public class Hero {
 
     public static void main(String[] args) {
         Hero h = new Hero();
+        h.name = "盖伦";
         ADHero ad = new ADHero();
         APHero ap = new APHero();
         ADAHero ada = new ADAHero();
         h.kill(ad);
         h.kill(ap);
         h.kill(ada);
+        System.out.println(h.toString());
+        //直接打印对象就是打印该对象的toString()返回值
+        System.out.println(h);
 
     }
 }
--- src/property/Item.java
+++ src/property/Item.java
@@ -10,4 +10,21 @@ public class Item {
     public void effect() {
         System.out.println("物品使用后，可以有效果");
     }
+    @Override
+    public String toString(){
+        return name+price;
+    }
+    @Override
+    public void finalize(){
+        System.out.println(name+"正在被回收");
+    }
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof Item){
+            Item i = (Item)o;
+            if(i.price == this.price )
+                return true;
+        }
+        return false;
+    }
 }
